00083d673a9ed8f7519cdc24382420f2be9860cc
Sebastian Ratz <ratz@AFZB.DE>
2016-06-15 11:18:33 +0200

    'leaving'

--- database/surveys.sql
+++ database/surveys.sql
@@ -3,7 +3,6 @@ DROP TABLE IF EXISTS surveys CASCADE;
 CREATE TABLE surveys (
 survey_id SERIAL PRIMARY KEY,
 surveytype_id INTEGER REFERENCES surveytypes ON UPDATE CASCADE ON DELETE CASCADE,
-name TEXT,
 place TEXT,
 surveydate Text,
 style_id INTEGER
